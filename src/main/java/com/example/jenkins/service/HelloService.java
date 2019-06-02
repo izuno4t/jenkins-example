ae298e078891b33c080cb777046e39be7aca7d59
tadashi izuno <tadashi.izuno@gmail.com>
2019-06-02 10:33:53 +0900

    update

---
+++ src/main/java/com/example/jenkins/service/HelloService.java
@@ -0,0 +1,37 @@
+package com.example.jenkins.service;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * サンプルのサービスクラスです。
+ * 
+ * @author izuno
+ */
+public class HelloService {
+
+	/**
+	 * 挨拶文の文字列を返します。
+	 * 
+	 * @return 挨拶文を返します。
+	 */
+	public String sayHello() {
+		if (true) {
+			if (true) {
+				if (false) {
+					return "aaa";
+				}
+			}
+		}
+		return "hello";
+	}
+
+	/**
+	 * ハッピーな文字列を返します。
+	 * 
+	 * @return ハッピーの文字列
+	 */
+	public String sayHappy() {
+		return StringUtils.upperCase("happy");
+	}
+
+}
